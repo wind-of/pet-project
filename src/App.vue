a63c3c01d5a576ba2afd17c3081d5f61ffdcf96b
wind-of <adequation@mail.ru>
2019-12-26 20:22:53 +0300

    refactor: removed unnecessary properties

--- src/App.vue
+++ src/App.vue
@@ -18,10 +18,4 @@
   a{
     text-decoration: none;
   }
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-  }
 </style>
